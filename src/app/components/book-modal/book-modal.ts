2d771b00527bf8ce15f3ed0fa0959e023c99b6fb
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-02-24 01:17:01 +0530

    Initial commit

---
+++ src/app/components/book-modal/book-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-modal',
+  imports: [],
+  templateUrl: './book-modal.html',
+  styleUrl: './book-modal.css',
+})
+export class BookModal {
+
+}
---
+++ src/app/components/cancel-modal/cancel-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel-modal',
+  imports: [],
+  templateUrl: './cancel-modal.html',
+  styleUrl: './cancel-modal.css',
+})
+export class CancelModal {
+
+}
