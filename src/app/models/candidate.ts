e3fe96d2769cd05b8c597b0fe426635adb827681
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-02-24 01:28:51 +0530

    candidate model ts completed

--- src/app/models/candidate.ts
+++ src/app/models/candidate.ts
@@ -1,2 +1,12 @@
-export class Candidate {
+export interface Candidate {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export interface CandidateRequest {
+  name: string;
+  email: string;
+  phone: string;
 }
