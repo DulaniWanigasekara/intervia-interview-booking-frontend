ca348ebc885bfd3cf38f9663c9c90346bdc3db4e
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-02-24 23:42:56 +0530

    book-modal ts and html updated

--- src/app/shared/components/book-modal/book-modal.ts
+++ src/app/shared/components/book-modal/book-modal.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Slot } from '../../../core/models/slot.model';
+import { StatusBadgeComponent } from '../status-badge/status-badge';
 
 @Component({
   selector: 'app-book-modal',
-  imports: [],
-  templateUrl: './book-modal.html',
-  styleUrl: './book-modal.css',
+  standalone: true,
+  imports: [CommonModule, StatusBadgeComponent],
+  templateUrl: './book-modal.html'
 })
-export class BookModal {
+export class BookModalComponent {
+  @Input() slot: Slot | null = null;
+  @Input() loading: boolean = false;
+  @Input() errorMessage: string = '';
 
+  @Output() confirmed = new EventEmitter<void>();
+  @Output() cancelled = new EventEmitter<void>();
+
+  confirm() { this.confirmed.emit(); }
+  close() { this.cancelled.emit(); }
 }
