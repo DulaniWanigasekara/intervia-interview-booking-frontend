5d5e5e74afe114657923b44b658d1065ee7f5368
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-02-24 22:26:46 +0530

    app routes ts updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.HomeComponent)
+  },
+  {
+    path: 'slots',
+    loadComponent: () =>
+      import('./pages/slots/slots').then(m => m.SlotsComponent)
+  },
+  {
+    path: 'my-booking',
+    loadComponent: () =>
+      import('./pages/my-booking/my-booking').then(m => m.MyBookingComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
